27c82d5d1a2596858ece3ce500e448f0b726401a
Nadhif Haikal Farhansyah <neorocket64@gmail.com>
2019-02-22 17:27:59 +0700

    Finalisation
    hope its done in time

--- praktikum_1_5.sh
+++ praktikum_1_5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat /var/log/syslog | awk '!/sudo/' && '/cron/' | awk 'NF < 13' >> /home/ugarpac/modul1
+cat /var/log/syslog | awk '!/sudo/ && /cron/' | awk 'NF < 13' >> /home/ugarpac/modul1/$(date +%S%M%H%d%m%Y).log
 #cat untuk melihat isi dari suatu file (as long as readable)
 #log tidak berisi command sudo, tapi berisi command cron
 #NF (Number of Field), sesuai dengan keinginan
